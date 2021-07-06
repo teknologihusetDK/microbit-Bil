67cd13311233121e8be07d894724c4a48dfffcb8
Jørgen Christian Larsen <39121814+jclarsen-th@users.noreply.github.com>
2021-07-06 15:25:23 +0200

    Update ReceiverIR.cpp

--- ReceiverIR.cpp
+++ ReceiverIR.cpp
@@ -272,7 +272,7 @@ void ReceiverIR::isr_rise(void) {
                  * By a model only?
                  * Please check a specification of your remote controller if you find a problem.
                  */
-                if (8 <= data.bitcount) {
+                if (2 <= data.bitcount) {
                     work.state = Received;
                     work.c1 = -1;
                     work.c2 = -1;
